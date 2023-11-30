39f68c9ce9c0250fc6c65627c6a4b88f00b4c429
Akinwande Aaron <aaronakinwande@gmail.com>
2023-11-30 15:12:11 +0100

    all input fields type using CVA

---
+++ components/shared/input/Input.tsx
@@ -0,0 +1,27 @@
+import { cva, VariantProps } from "class-variance-authority";
+
+const inputStyles = cva("rounded-md font-medium h-full w-full border focus:outline-teal-200 hover:border-teal-200 disabled:bg-gray-100 ", {
+    variants: {
+      size: {
+        small: "py-1 px-6 text-sm",
+        large: "py-2 px-6 text-base",
+      },
+      defaultVariants: {
+        size: "base"
+      },
+    },
+  });
+  interface InputProps extends VariantProps<typeof inputStyles> {
+    text: string;
+    error:boolean|null;
+    success:boolean|null;
+    disabled:boolean;
+  }
+  export default function Button({ size, text,disabled,error,success, ...props }: InputProps) {
+ 
+    return (
+      <input className={`${inputStyles({ size })} ${error ? "border-red-500" : null} ${success ? "border-green-500" : null}`}
+       {...props}  disabled={disabled} placeholder={text}/>    
+      
+    );
+  }
