7debec9ed9178c6ac85655c3f16f8b7fcd973438
dorcas <adeboyedorcas67@gmail.com>
2023-12-01 12:34:15 +0100

    created he mobile view for the sidebar component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -23,15 +23,15 @@ export default function () {
        }
       ];
   return (
-        <div className=' bg-[#0065C2] w-1/5 px-6 py-8 h-screen fixed'>
-            <p className='text-white font-bold text-xl pb-8'>SMS</p>
-            <div className=' flex flex-col gap-5'>
+        <div className=' bg-[#0065C2] w-full md:w-1/5 p-6 h-screen fixed bg-opacity-70 md:bg-opacity-90 flex flex-col gap-6 '>
+            <p className='text-white font-bold text-xl hidden md:block'>SMS</p>
+            <div className='flex flex-col gap-5 pt-10 md:pt-0'>
             {
             nav.map((item,index) => (
-                <div className="" key={index} >
-                      <button className='flex gap-2 text-white w-full font-medium items-center hover:bg-white hover:text-[#0065C2] p-3 rounded-lg'>
+                <div className="" key={index}>
+                      <button className='  flex gap-2 text-white w-full font-medium items-center hover:bg-white hover:text-[#0065C2] p-3 rounded-lg '>
                   <p className='text-2xl'>{item.icon}</p>
-                   <p className='text-base'>{item.name}</p>
+                   <p className='text-base '>{item.name}</p>
                       </button>                   
                 </div>
             ))
