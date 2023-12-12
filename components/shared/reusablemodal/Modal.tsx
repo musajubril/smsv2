6c9c158adcd334c53603c05f5df112f6ac5d4b56
Francis matthew <matthewabiodun2001@gmail.com>
2023-12-12 03:58:15 +0100

    removed the fixed height and width for the modal

--- components/shared/reusablemodal/Modal.tsx
+++ components/shared/reusablemodal/Modal.tsx
@@ -15,22 +15,18 @@ export default function Modal({ action, children, open }: ModalProps) {
     >
       <div className=" flex flex-col justify-end">
         <div
-          className="modal-container bg-white p-8 rounded-2xl  scrollbar-hidden xl:h-auto max-h-[calc(100vh-100px)] "
+          className="bg-white rounded-2xl xl:h-auto max-h-[calc(100vh-100px)] "
           style={{ height: "" }}
           onClick={(e) => e.stopPropagation()}
         >
-          <div className="modal-content md:w-[768px] md:h-[495px] w-auto h-auto gap-5 bg-[white] items-center rounded-xl">
+          <div className="gap-5 bg-[white] items-center rounded-xl">
             <div
               onClick={() => action(false)}
-              className=" flex w-auto justify-end hover:cursor-pointer pb-5"
+              className=" flex justify-end hover:cursor-pointer" 
             >
-              <img
-                src="cancel.svg"
-                alt="cancel button"
-                className=" h-10 w-10 pr-4"
-              />
+
             </div>
-            <div>{children}</div>
+            <div className="p-12">{children}</div>
           </div>
         </div>
       </div>
