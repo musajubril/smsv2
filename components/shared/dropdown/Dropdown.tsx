282c96e4b6e661bf8b1d445ec634eab071181c8b
Akinwande Aaron <aaronakinwande@gmail.com>
2024-01-09 09:55:00 +0100

    Added navigation to add student page

--- components/shared/dropdown/Dropdown.tsx
+++ components/shared/dropdown/Dropdown.tsx
@@ -43,12 +43,12 @@ export default function Dropdown({ options, onSelect, placeholder }){
         <IoIosArrowDown />
       </div>
       {isOpen && (
-        <div className="absolute z-10 top-full left-0 bg-white-100 shadow-lg rounded-md mt-2">
+        <div className="absolute top-full left-0 bg-white-100 shadow-lg rounded-md mt-2 outline-dashed">
           {options.map((option) => (
             <div
               key={option}
               onClick={() => handleSelect(option)}
-              className="cursor-pointer rounded-md py-2 px-4 hover:bg-gray-500"
+              className="cursor-pointer rounded-md py-2 px-2 hover:bg-gray-500"
             >
               {option}
             </div>
