05cea906c6fad6d5a8110f584e29ad3a4459f6bf
harbiodun-coder <matthewabiodun2001@gmail.com>
2024-01-29 08:32:46 +0000

    staff integration

--- components/StaffList.tsx
+++ components/StaffList.tsx
@@ -1,122 +1,83 @@
 import React from 'react'
 import { HiDotsVertical } from 'react-icons/hi'
 import { SlArrowLeft, SlArrowRight } from 'react-icons/sl'
+import { useQuery } from "@tanstack/react-query";
+import { TEACHERS } from '@/api/apiUrl';
+import { getRequest } from '@/api/apiCall';
+import { queryKeys } from '@/api/queryKey';
+import Router, { useRouter } from "next/router";
+import Link from "next/link";
 
-export default function StaffList() {
-  const info = [
-    {
-      image: <img src="./Avatars (2).png" alt="" />,
-      name: "Shakirat Akanji",
-      id: "ID:123456",
-      Department: "Academic",
-      Role: " Teacher",
-      Qualification: "Bsc",
-      Action : <HiDotsVertical />,
-      input : <input type="checkbox" name="" id="" />
-    },
-    {
-      image: <img src="./Avatars (2).png" alt="" />,
-      name: "Shakirat Akanji",
-      id: "ID:123456",
-      Department: "Academic",
-      Role: " Teacher",
-      Qualification: "Bsc",
-      Action : <HiDotsVertical />,
-      input : <input type="checkbox" name="" id="" />
-    },{
-      image: <img src="./Avatars (2).png" alt="" />,
-      name: "Shakirat Akanji",
-      id: "ID:123456",
-      Department: "Academic",
-      Role: " Teacher",
-      Qualification: "Bsc",
-      Action : <HiDotsVertical />,
-      input : <input type="checkbox" name="" id="" />
-    },{
-      image: <img src="./Avatars (2).png" alt="" />,
-      name: "Shakirat Akanji",
-      id: "ID:123456",
-      Department: "Academic",
-      Role: " Teacher",
-      Qualification: "Bsc",
-      Action : <HiDotsVertical />,
-      input : <input type="checkbox" name="" id="" />
-    },{
-      image: <img src="./Avatars (2).png" alt="" />,
-      name: "Shakirat Akanji",
-      id: "ID:123456",
-      Department: "Academic",
-      Role: " Teacher",
-      Qualification: "Bsc",
-      Action : <HiDotsVertical />,
-      input : <input type="checkbox" name="" id="" />
-    },{
-      image: <img src="./Avatars (2).png" alt="" />,
-      name: "Shakirat Akanji",
-      id: "ID:123456",
-      Department: "Academic",
-      Role: " Teacher",
-      Qualification: "Bsc",
-      Action : <HiDotsVertical />,
-      input : <input type="checkbox" name="" id="" />
-    },{
-      image: <img src="./Avatars (2).png" alt="" />,
-      name: "Shakirat Akanji",
-      id: "ID:123456",
-      Department: "Academic",
-      Role: " Teacher",
-      Qualification: "Bsc",
-      Action : <HiDotsVertical />,
-      input : <input type="checkbox" name="" id="" />
-    },
-    ]
+export default function StaffList({staffs}) {
+
+
+    const router = useRouter();
+    let school;
+    if (typeof window !== 'undefined') {
+      school = localStorage.getItem('schoolSlug');
+    }
   return (
     <div>
       <div className=' px-5  '>
-        <div className='bg-[#F5FAFF] p-2 grid grid-cols-6 text-xs font-medium text-[#000D19]'>
-         <div className=' flex gap-2 items-center  col-span-2 '>
-           <input type="checkbox" name="" id="" className=' w-5 h-5'/>
-           <h5 className=' text-xs font-medium text-[#344054]'>Name</h5>
+<div className=" grid grid-cols-6 text-sm bg-[#F5FAFF] p-3  rounded-t-lg">
+            <div className=" font-semibold text-xs flex gap-2 col-span-2">
+              <input type="checkbox" name="" id="" className=" h-5 w-5" />
+              Name
+            </div>
+            <div className=" font-semibold text-xs col-span-1">Department</div>
+            <div className=" font-semibold text-xs col-span-1">Role</div>
+            <div className=" font-semibold text-xs col-span-1">
+              Qualification
+            </div>
+            <div className=" font-semibold text-xs col-span-1 flex justify-center">
+              Action
+            </div>
           </div>
-          <h1>Department</h1>
-          <h1>Role</h1>
-          <h1>Qualification</h1>
-          <h1 className='flex items-center justify-center'>Action</h1>
-          
 
-</div>
+<div className="flex flex-col gap-3 py-2 px-3">
+            {
+              staffs &&
+                staffs.map((teachers) => (
+                  <div
+                    key={teachers.id}
+                    className=" grid grid-cols-6 font-semibold text-sm "
+                  >
+                    <div className=" col-span-2 flex items-center gap-3">
+                      <div className="">
+                        <input
+                          type="checkbox"
+                          name=""
+                          id=""
+                          className=" h-5 w-5"
+                        />
+                      </div>
+                      <div className=" border h-8 w-8 rounded-full overflow-hidden">
+                        <img
+                          src={teachers.image}
+                          alt=""
+                          className=" object-cover "
+                        />
+                      </div>
+                  
+                      <Link href={`/${school}/staff/${teachers.id}/`}>
+                       <div className="">
+                       <p>{teachers.full_name}</p>  
+                      <p className=" font-normal text-xs text-[#475367]">ID:{teachers.id}</p>
+                      </div>
+                      </Link>
+                    </div>
+                    <div className="col-span-1">{teachers.role}</div>
+                    <div className="col-span-1 text-gray-400">..</div>
 
-    {
-      info.map((info, index) => (
-          <div className="  " key={index}>
-             
-              
-          <div className=' grid grid-cols-6 pt-5 items-center  px-3 grid2'>
-                             <div className=' flex items-center gap-2 col-span-2'>
-                              {info.input}
-                  <div>{info.image}</div>
-                  <div className='flex flex-col'>
-                      <h1 className='text-sm font-medium text-[#101928]'>{info.name}</h1>
-                      <h1 className='text-xs text-[#475367]' >{info.id}</h1>
-          
+                    <div className=" cursor-pointer flex justify-center">
+                      <HiDotsVertical />
+                    </div>
                   </div>
-              </div>
-              <div className=' flex-shrink-0'>
-              <div className='text-sm font-medium text-[#04326B] bg-[#E3EFFC] px-2  rounded-lg inline-grid '>{info.Department}</div>
-              </div>
-              <div>{info.Role}</div>
-              <div >{info.Qualification}</div>
-          
-              <div className=' flex justify-center items-center'>
-                {info.Action}
-          
-              </div> 
-                              
-              </div>
+                ))
+            }
           </div>
-    
-      ))
-    }
+
+
     <div className="flex items-center justify-center pt-5">
           <div className=" flex items-center gap-3">
             <div className=" border border-[#D0D5DD] rounded-md p-2 shadow">
