c77b8c38e7b18f2390937ad6cc7df1d1e6218930
dorcas <adeboyedorcas67@gmail.com>
2024-06-10 11:08:18 +0100

    workes on responsiveness for the pages i've worked on

--- components/shared/Charts/Barchart_students.tsx
+++ components/shared/Charts/Barchart_students.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { IoMdArrowDropdown } from 'react-icons/io';
 
-const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const labels = ['2001', '2002', '2003', '2004', '2005', '2006', '2007'];
 
 const data = {
   labels,
   datasets: [
     {
-      data: [800, 600, 340, 334, 400, 500, 400, 330, 221, 75, 500, 400],
+      data: [1500, 500, 1800, 3000, 2000, 2300, 400, 1500],
       borderRadius: 10,
-      backgroundColor: labels.map((month) => (month === 'Jun' ? 'rgba(0, 101, 194, 1)' : 'rgba(194, 226, 254, 1)')),
+      backgroundColor: labels.map((year) => (year === '2004' ? 'rgba(0, 101, 194, 1)' : 'rgba(194, 226, 254, 1)')),
+      barThickness: 35,
     },
   ],
 };
@@ -19,9 +20,15 @@ const data = {
 const options = {
   plugins: {
     legend: {
+      display: false,
+    },
+  },
+  scales: {
+    x: {
+      grid: {
         display: false,
-    }
- 
+      },
+    },
   },
 };
 
@@ -29,28 +36,32 @@ const Barchart = () => {
   return (
     <>
       <div className="">
-        <div className='p-4 rounded-lg bg-white-100 shadow'>
-          <div className='flex flex-col gap-5'>
-            <div className='flex justify-between'>
-              <div className='flex flex-col gap-2'>
-                <h1 className='text-xs md:text-lg text-[#9291A5]'> Students</h1>
-                <h1 className='text-sm md:text-xl text-[#000D19] font-semibold'>Total Students</h1>
+        <div className="p-4 rounded-lg bg-white-100 shadow">
+          <div className="flex flex-col gap-5">
+            <div className="flex justify-between">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-xs md:text-lg text-[#9291A5]">Students</h1>
+                <h1 className="text-sm md:text-xl text-[#000D19] font-semibold">Total Students</h1>
               </div>
               <div>
-                <div className='flex gap-5'>
-                  <div className='py-2 flex text-[#000D19] text-xs md:text-sm font-medium items-center border-[#C2E2FF] rounded-lg border px-4 gap-1'>
+                <div className="flex gap-5">
+                  <div className="py-2 flex text-[#000D19] text-xs md:text-sm font-medium items-center border-[#C2E2FF] rounded-lg border px-4 gap-1">
                     Total
-                    <h1 className='md:text-xl'> <IoMdArrowDropdown /></h1>
+                    <h1 className="md:text-xl">
+                      <IoMdArrowDropdown />
+                    </h1>
                   </div>
-                  <div className='py-2 flex text-[#000D19] text-xs md:text-sm font-medium items-center border-[#C2E2FF] rounded-lg border px-4 gap-1'>
+                  <div className="py-2 flex text-[#000D19] text-xs md:text-sm font-medium items-center border-[#C2E2FF] rounded-lg border px-4 gap-1">
                     Yearly
-                    <h1 className='md:text-xl'> <IoMdArrowDropdown /></h1>
+                    <h1 className="md:text-xl">
+                      <IoMdArrowDropdown />
+                    </h1>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          
+
           <Bar data={data} options={options} />
         </div>
       </div>
--- pages/parental_access/login.tsx
+++ pages/parental_access/login.tsx
@@ -9,26 +9,26 @@ export default function Onboarding () {
   return (
     <div className='  '>
     <div className=' md:bg-[#F9FAFB] bg-white-100  h-screen '>
-        <div className=' flex justify-center items-center flex-col p-4 h-screen '>
+        <div className=' flex justify-center items-center flex-col px-4 h-screen '>
           <h1 className=' text-2xl text-[#0065C2] font-bold pb-10'>SMS</h1>
           <div className='flex flex-col gap-5 justify-center items-center'>
-            <div className=' flex md:hidden'><img src="./emoji.png" alt="" className=' w-20 h-20' /></div>
+            <div className=' flex md:hidden'><img src="/emoji.png" alt="" className=' w-20 h-20' /></div>
             <div className=' flex flex-col gap-2 text-center '>
-                <h1 className='text-3xl font-semibold text-[BABABA]'><span className='text-[#000D19]'>Login.</span>Parents/Guardian</h1>
-                <p>Thank you for bringing your wards/children to our school</p>
+                <h1 className='text-xl md:text-3xl font-semibold text-[BABABA]'><span className='text-[#000D19]'>Login.</span>Parents/Guardian</h1>
+                <p className=' text-sm md:text-lg'>Thank you for bringing your wards/children to our school</p>
 
             </div>
             <div className=' w-full flex flex-col gap-5'>
             <div className=' flex flex-col'>
-            <div>Username:</div>
+            <div className=' text-sm md:text-lg'>Username:</div>
             <Input text={'Enter Subject'} name={'Username'} error={false} success={false} disabled={false} size='large' value={undefined} change={function (event: React.ChangeEvent<HTMLInputElement>): void {
            
                 } }  />
                   </div>
                   <div>
-          <div>Password:</div>
+          <div className='text-sm md:text-lg'>Password:</div>
           <div className=''>
-                <PasswordInput text={'Enter Subject'} name={''} size='large' change={undefined} value={undefined} disabled={false} error={false} success={false}  />
+                <PasswordInput text={'Enter Password'} name={''} size='large' change={undefined} value={undefined} disabled={false} error={false} success={false}  />
                 </div>
                 </div>
                 <Link href={'/parental_access/profile'}>
